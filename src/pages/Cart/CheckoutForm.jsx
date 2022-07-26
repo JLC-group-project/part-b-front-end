3dcf9d7ed263bceb458d9b7c2b5f1fd1323c45b5
Jimmy <jameswang675203@gmail.com>
2022-07-26 16:32:58 +1000

    deleted some code in checkout form

--- src/pages/Cart/CheckoutForm.jsx
+++ src/pages/Cart/CheckoutForm.jsx
@@ -1,11 +1,5 @@
 import React, { useState } from "react";
-import { Elements, CardElement } from "@stripe/react-stripe-js";
-import { loadStripe } from "@stripe/stripe-js";
 
-
-  const stripe = loadStripe(
-    "pk_live_51L13DBC2W0jHLBfIED01SG0CllozsvMy9dcUsRT87BxaMsfKn2kXrf1RitrWPjC5BNDoEQpAoDv9KA8SWnSvknWQ00zAMoOaC6"
-  );
 
 function CheckoutForm() {
   <Elements stripe={stripe}>
